bcad7092ba2121ab001cbc05503e0a22e26ee3dd
Justin Fanton <justin.fanton@gmail.com>
2024-10-23 11:49:55 +0200

    Code along - implemented cards + movie array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import Footer from "./components/Footer";
+import "./index.css"
 
 function App() {
   return (
     <>
-    
+      <Header />
+      <MovieList />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
